1785172b1a23b3807c2787776ebd4a144aa7e237
Angela J. Holden <angelajholden@gmail.com>
2020-12-30 13:23:12 -0600

    update uglify for es6

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,20 +1,20 @@
 function openCloseMenu() {
-    let icon = document.getElementById("menu_icon");
-    let menu = document.getElementById("menu_drawer");
+	let icon = document.getElementById("menu_icon");
+	let menu = document.getElementById("menu_drawer");
 
-    icon.addEventListener("click", function (e) {
-        let clicked = e.target.classList.contains("clicked");
+	icon.addEventListener("click", (e) => {
+		let clicked = e.target.classList.contains("clicked");
 
-        if (clicked === false) {
-            icon.classList.add("clicked");
-            menu.classList.add("active");
-        } else if (clicked === true) {
-            icon.classList.remove("clicked");
-            menu.classList.remove("active");
-        }
-    });
+		if (clicked === false) {
+			icon.classList.add("clicked");
+			menu.classList.add("active");
+		} else if (clicked === true) {
+			icon.classList.remove("clicked");
+			menu.classList.remove("active");
+		}
+	});
 }
 
-window.onload = function () {
-    openCloseMenu();
+window.onload = () => {
+	openCloseMenu();
 };
